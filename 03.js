fa9902e3094f7ddb96e4b6a6131cdc9cf2428db5
vetsinen <vetsinen@gmail.com>
2021-08-30 13:21:43 +0300

    initial commit

---
+++ 03.js
@@ -0,0 +1,23 @@
+const b = {}
+function bar(){ return {}}
+const arr = [1,2,3,4,5]
+const n = null
+const u = undefined
+
+console.log(typeof b)
+console.log(b)
+
+console.log(typeof bar)
+console.log(bar)
+
+console.log(typeof bar())
+console.log(bar())
+
+console.log(typeof arr)
+console.log(arr)
+
+console.log(typeof n)
+console.log(n)
+
+console.log(typeof u)
+console.log(u)
---
+++ 06.js
@@ -0,0 +1,6 @@
+const swarmLearning = {name:"Swarm Learning", date:"2021-08-30", fee: 42}
+
+for (let prop in swarmLearning) {
+    console.log(prop)
+    console.log("obj." + prop + " = " + swarmLearning[prop]);
+}
