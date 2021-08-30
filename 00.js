75a43e71ee9ee29e89841eb9066fc2e3fc4e3a26
vetsinen <vetsinen@gmail.com>
2021-08-30 19:08:26 +0300

    added fields dublicate example

--- 00.js
+++ 00.js
@@ -3,15 +3,19 @@ const fee = 100
 console.log(typeof party)
 console.log(typeof fee)
 
-const swarmLearning = {name:"Swarm Learning", date:"2021-08-30", fee: 0}
-console.log(typeof swarmLearning)
-console.log(swarmLearning)
+const sw = {name:"Swarm Learning", date:"2021-08-30", fee: 0}
+console.log(typeof sw)
+console.log(sw)
+// const sw2 = {name:"Swarm Learning", date:"2021-08-30", fee: 0, fee:100}
+//does it work?
 
 const visitor = {"name":"John","promocode":"A-level101"}
 console.log(typeof visitor)
 console.log(visitor)
 
-// console.log(swarmLearning.name)
-// console.log(swarmLearning["date"])
-// swarmLearning.priority = 4
-// console.log(swarmLearning)
+console.log(sw.name)
+console.log(sw["date"])
+
+sw.priority = 4
+sw.minAge = 18
+console.log(sw)
